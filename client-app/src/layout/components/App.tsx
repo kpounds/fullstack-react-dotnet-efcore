932193ac4884e33351976a3a93273faf69cb6be1
Keith Pounds <kpounds@greenmountaintechnology.com>
2021-03-02 15:21:48 -0600

    using a GUID for the activity id

--- client-app/src/layout/components/App.tsx
+++ client-app/src/layout/components/App.tsx
@@ -4,6 +4,7 @@ import { Container } from "semantic-ui-react"
 import { IActivity } from "../../models/activity"
 import NavBar from "./NavBar"
 import ActivityDashboard from "../../pages/dashboard/ActivityDashboard"
+import { v4 as uuid } from "uuid"
 
 function App() {
   const [activities, setActivities] = useState<IActivity[]>([])
@@ -30,7 +31,7 @@ function App() {
   function handleCreateOrEditActivity(activity: IActivity) {
     activity.id
       ? setActivities([...activities.filter((x) => x.id !== activity.id), activity])
-      : setActivities([...activities, activity])
+      : setActivities([...activities, { ...activity, id: uuid() }])
     setEditMode(false)
     setSelectedActivity(activity)
   }
