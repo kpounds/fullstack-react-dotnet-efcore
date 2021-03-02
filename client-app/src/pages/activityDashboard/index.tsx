5c4eec61e168e50651eb72f3633b879dfb8b674d
Keith Pounds <kpounds@greenmountaintechnology.com>
2021-03-02 11:29:57 -0600

    creating details view

--- client-app/src/pages/activityDashboard/index.tsx
+++ client-app/src/pages/activityDashboard/index.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent } from "react"
 import { Grid } from "semantic-ui-react"
 import { IActivity } from "../../models/activity"
+import ActivityDetails from "../activityDetails"
 import ActivityList from "./components/ActivityList"
 
 interface IActivityDashboardProps {
@@ -13,6 +14,7 @@ const ActivityDashboard: FunctionComponent<IActivityDashboardProps> = ({ activit
       <Grid.Column width="10">
         <ActivityList activities={activities} />
       </Grid.Column>
+      <Grid.Column width="6">{activities[0] && <ActivityDetails activity={activities[0]} />}</Grid.Column>
     </Grid>
   )
 }
