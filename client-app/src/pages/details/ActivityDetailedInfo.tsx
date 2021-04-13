15b5af814b1ed8f5e4c3eb7cc0069b1be2e5feee
Keith Pounds <kpounds@greenmountaintechnology.com>
2021-04-13 14:43:27 -0500

    Populating the detailed components

--- client-app/src/pages/details/ActivityDetailedInfo.tsx
+++ client-app/src/pages/details/ActivityDetailedInfo.tsx
@@ -1,7 +1,48 @@
-import React from "react"
+import { observer } from "mobx-react-lite"
+import { Segment, Grid, Icon } from "semantic-ui-react"
+import { IActivity } from "../../models/activity"
 
-const ActivityDetailedInfo = () => {
-  return <h1>Info</h1>
+interface Props {
+  activity: IActivity
 }
 
-export default ActivityDetailedInfo
+const ActivityDetailedInfo = ({ activity }: Props) => {
+  return (
+    <Segment.Group>
+      <Segment attached="top">
+        <Grid>
+          <Grid.Column width={1}>
+            <Icon size="large" color="teal" name="info" />
+          </Grid.Column>
+          <Grid.Column width={15}>
+            <p>{activity.description}</p>
+          </Grid.Column>
+        </Grid>
+      </Segment>
+      <Segment attached>
+        <Grid verticalAlign="middle">
+          <Grid.Column width={1}>
+            <Icon name="calendar" size="large" color="teal" />
+          </Grid.Column>
+          <Grid.Column width={15}>
+            <span>{activity.date}</span>
+          </Grid.Column>
+        </Grid>
+      </Segment>
+      <Segment attached>
+        <Grid verticalAlign="middle">
+          <Grid.Column width={1}>
+            <Icon name="marker" size="large" color="teal" />
+          </Grid.Column>
+          <Grid.Column width={11}>
+            <span>
+              {activity.venue}, {activity.city}
+            </span>
+          </Grid.Column>
+        </Grid>
+      </Segment>
+    </Segment.Group>
+  )
+}
+
+export default observer(ActivityDetailedInfo)
