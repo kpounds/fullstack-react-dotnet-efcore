e22c1d02b7c1a77c7d870c235a8595e72fd538e2
Keith Pounds <kpounds@greenmountaintechnology.com>
2021-04-13 15:23:56 -0500

    Styling the home page

--- client-app/src/pages/dashboard/components/ActivityListItem.tsx
+++ client-app/src/pages/dashboard/components/ActivityListItem.tsx
@@ -1,23 +1,12 @@
-import { SyntheticEvent, useState } from "react"
 import { Link } from "react-router-dom"
 import { Button, Icon, Item, Segment } from "semantic-ui-react"
 import { IActivity } from "../../../models/activity"
-import { useStore } from "../../../stores/rootStore"
 
 interface IActivityListItemProps {
   activity: IActivity
 }
 
 const ActivityListItem = ({ activity }: IActivityListItemProps) => {
-  const { activityStore } = useStore()
-  const { deleteActivity, loading } = activityStore
-  const [target, setTarget] = useState("")
-
-  function handleDeleteActivity(e: SyntheticEvent<HTMLButtonElement>, id: string) {
-    setTarget(e.currentTarget.name)
-    deleteActivity(id)
-  }
-
   return (
     <Segment.Group>
       <Segment>
